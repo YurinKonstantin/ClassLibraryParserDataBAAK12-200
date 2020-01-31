e3ef8dcec115b6bd7b68e76e864bb824868bca4b
Константин Юрин <yurinkonstantin@mail.ru>
2020-01-31 14:11:48 +0300

    Исправление в подсчета суммарной плащади

--- ClassLibrary3/Class1.cs
+++ ClassLibrary3/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +16,22 @@ namespace ParserBAAK12
         /// <param name="xLenght"></param>
         /// <param name="data"></param>
         /// <param name="TimeData"></param>
-        public static void ParseBinFileBAAK200(Byte[] buf00, int xLenght, out Int32[,] data, out string TimeData)
+        public static void ParseBinFileBAAK200(Byte[] buf00, int xLenght, out int[,] data, out string TimeData)
         {
+            data = new int[12, 1024];
+            if (buf00.Length== 504648)
+            {
+                data = new int[12, 1024];
+                xLenght = 1;
 
-            data = new int[12, (int)(buf00.Length/12)];
 
+
+            }
+            if(buf00.Length> 504650)
+            {
+                data = new int[12, 2048];
+                xLenght = 2;
+            }
             TimeData = String.Empty;
 
 
@@ -29,20 +41,21 @@ namespace ParserBAAK12
                 aTime[d] = buf00[12 + d];
             }
 
-            TimeData =MyParseBinFileData(aTime);
+            TimeData = MyParseBinFileData(aTime);
             for (int i = 0; i < 12; i++)
             {
 
 
                 for (int j = 0; j < (1024 * xLenght); j++)
                 {
-                   
-                    data[i, j] = (((buf00[25 + (j * 2) + (i * 2048 * xLenght)] & 0xf) << 8) | buf00[24 + (j * 2) + (i * 2048 * xLenght)]);
+
+                    data[i, j] =Convert.ToInt16( (((buf00[25 + (j * 2) + (i * 2048 * xLenght)] & 0xf) << 8) | buf00[24 + (j * 2) + (i * 2048 * xLenght)]));
 
                 }
 
 
             }
+
 
         }
         /// <summary>
@@ -60,35 +73,40 @@ namespace ParserBAAK12
         /// <param name="obrNoise">Порого срабатывания канала</param>
         public static void MaxAmpAndNul(int[,] data1, ref double[] sig, ref int[] Amp, ref double[] Nul, ref bool bad, bool obrNoise, double KoefNoise, int AmpNoise)
         {
-           // bad = false;
-           // sig = new Double[12];
-           // Amp = new int[12];
+            // bad = false;
+            // sig = new Double[12];
+            // Amp = new int[12];
             double min;
             int xbad = 0;
             //bool obrNoise = ClassUserSetUp.ObrNoise;
             // Nul = new double[12];
             int Nsob = 150;//число точек от начала для поиска нулевой линнии
-            int[] sumNul = new int[Nsob];
+           
             
             for (int i = 0; i < 12; i++)
             {
-               // точки нулевой линии для "a" - го канала
-                                             // double sred = Enumerable.Range(0, 2).Select(x => f[x]).Sum();
-              //  for (int a = 0; a < Nsob; a++)
-              //  {
-                    // Nul[i] = (Nul[i] + data1[i, a]);
-                 //   sumNul[a] = data1[i, a];// точки нулевой линии для "a"-го канала
-               // }
-                int[] p = Enumerable.Range(0, 150).Select(x => data1[i, x]).ToArray();
-                int nul = (int)p.Average();
-                Nul[i] = nul;
-                sig[i] = Math.Sqrt(Sum(p, nul) / Nsob);
+               
+               // int[] p = Enumerable.Range(0, 150).Select(x => data1[i, x]).ToArray();
+              double[] p = new double[150];
+                for(int f=0; f<150; f++)
+                {
+                    p[f] = data1[i, f];
+                }
 
-                int aa = (Enumerable.Range(0, (data1.Length / 12)).Select(x => data1[i, x]).Max()) - nul;
+                double nul = p.Sum()/150.0;
+                Nul[i] = nul;
+                sig[i] =Convert.ToInt16( Math.Sqrt(Sum(p, nul) / (Nsob-1)));
+                int[] masZ = new int[data1.Length / 12];
+                for(int s=0; s< data1.Length / 12; s++)
+                {
+                    masZ[s] = data1[i, s];
+                }
+                int aa =masZ.Max() -Convert.ToInt16(nul);
+      
                 Amp[i] = aa;
                 min = (Enumerable.Range(0, 1024).Select(x => data1[i, x]).Min()) - nul;
 
-                if ((obrNoise && (Math.Abs(min / aa) > KoefNoise) && Math.Abs(aa) > AmpNoise) || Nul[i]<2000)
+                if ((obrNoise && (Math.Abs(min / aa) >= KoefNoise) && Math.Abs(aa) > AmpNoise) || Nul[i]<2000)
                 {
                     xbad++;
                 }
@@ -100,22 +118,22 @@ namespace ParserBAAK12
                 bad = true;
             }
         }
-        static double  sum1D(double[] masD)
+        static double sum1D(double[] masD)
         {
-            return Enumerable.Range(300, 700).Select(x => masD[x]).Sum();
+            return Enumerable.Range(320, 704).Select(x => masD[x]).Sum();
 
         }
         public static void SumSig(double[,] data1, out double[] sumsin)
         {
 
             //int[] sumD = new int[1024];
-            double[] mas1 = new Double[1024];
+            double[] mas1 = new double[1024];
             sumsin = new double[12];
             for (int i = 0; i < 12; i++)
             {
 
                 // double sred = Enumerable.Range(0, 2).Select(x => f[x]).Sum();
-                for (int j = 0; j < 1014; j++)
+                for (int j = 0; j < 1024; j++)
                 {
                     mas1[j] = data1[i, j];
                    
@@ -131,12 +149,12 @@ namespace ParserBAAK12
           
 
         }
-        private static double Sum(int[] n, double x)
+        private static double Sum(double[] n, double x)
         {
-            double res=0;
+            double res =0;
            for (int i=0; i<n.Length; i++)
             {
-                res = res + Math.Pow((n[i] - x), 2);
+                res = res +Math.Pow((n[i] - x), 2);
             }
             return res;
         }
@@ -155,36 +173,46 @@ namespace ParserBAAK12
             try
             {
                 byte[] aTime = new byte[8];
-               // for (int d = 0; d < 8; d++)
-               // {
-               //     aTime[d] = buf00[480032+ d];
+                // for (int d = 0; d < 8; d++)
+                // {
+                //     aTime[d] = buf00[480032+ d];
                 //}
-                aTime[0] = buf00[480032];
-                aTime[1] = buf00[480033];
-                aTime[2] = buf00[480034];
-                aTime[3] = buf00[480035];
-                aTime[4] = buf00[480036];
-                aTime[5] = buf00[480037];
-                aTime[6] = buf00[480038];
-                aTime[7] = buf00[480039];
-                TimeData = MyParseBinFileData(aTime);
-                 for (int i = 0; i < 12; i++)
-                 {
-                     for (int j = 0; j < 20000; j++)
-                     {
-
-                         dataTail[i, j] = (((buf00[25 + (i * 2) + (j * 24)] & 0xf) << 8) | buf00[24 + (i * 2) + (j * 24)]);
-
-                     }
-                     for (int j = 0; j < (1024); j++)
-                     {
-                         data[i, j] = (((buf00[480065 + (j * 2) + (i * 2048)] & 0xf) << 8) | buf00[480064 + (j * 2) + (i * 2048)]);
-                     }
+                if (buf00.Count() > 480039)
+                {
 
 
-                 }
-             
-                return "1";
+                    aTime[0] = buf00[480032];
+                    aTime[1] = buf00[480033];
+                    aTime[2] = buf00[480034];
+                    aTime[3] = buf00[480035];
+                    aTime[4] = buf00[480036];
+                    aTime[5] = buf00[480037];
+                    aTime[6] = buf00[480038];
+                    aTime[7] = buf00[480039];
+
+                    TimeData = MyParseBinFileData(aTime);
+                    for (int i = 0; i < 12; i++)
+                    {
+                        for (int j = 0; j < 20000; j++)
+                        {
+
+                            dataTail[i, j] = Convert.ToInt16(((buf00[25 + (i * 2) + (j * 24)] & 0xf) << 8) | buf00[24 + (i * 2) + (j * 24)]);
+
+                        }
+                        for (int j = 0; j < (1024); j++)
+                        {
+                            data[i, j] = Convert.ToInt16(((buf00[480065 + (j * 2) + (i * 2048)] & 0xf) << 8) | buf00[480064 + (j * 2) + (i * 2048)]);
+                        }
+
+
+                    }
+
+                    return "1";
+                }
+                else
+                {
+                    return "ochibka";
+                }
             }
             catch(Exception ex)
             {
@@ -195,15 +223,22 @@ namespace ParserBAAK12
 
         private static string MyParseBinFileData(Byte[] bytTime)
         {
-           
 
-          //  string dd = (((bytTime[6] & 0x03) << 4) | (bytTime[5] >> 4)).ToString("00");
 
-          
-            return (((bytTime[6] & 0x03) << 4) | (bytTime[5] >> 4)).ToString("00") + "." + (((bytTime[5] & 0x0f) << 1) | (bytTime[4] >> 7)).ToString("00") + "." + ((bytTime[4] >> 1) & 0x3f).ToString("00") + "." + (((bytTime[4] & 0x01) << 5) | (bytTime[3]) >> 3).ToString("00")
-                + "." + (((bytTime[3] & 0x07) << 7) | ((bytTime[2] >> 1) & 0x7f)).ToString("000")
-                + "." + (((bytTime[2] & 0x1) << 9) | (bytTime[1] << 1) | (bytTime[0] >> 7)).ToString("000") + "." + (((bytTime[0] & 0x7f)) * 10).ToString("000"); ;
+            //  string dd = (((bytTime[6] & 0x03) << 4) | (bytTime[5] >> 4)).ToString("00");
 
+            try
+            {
+
+
+                return (((bytTime[6] & 0x03) << 4) | (bytTime[5] >> 4)).ToString("00") + "." + (((bytTime[5] & 0x0f) << 1) | (bytTime[4] >> 7)).ToString("00") + "." + ((bytTime[4] >> 1) & 0x3f).ToString("00") + "." + (((bytTime[4] & 0x01) << 5) | (bytTime[3]) >> 3).ToString("00")
+                    + "." + (((bytTime[3] & 0x07) << 7) | ((bytTime[2] >> 1) & 0x7f)).ToString("000")
+                    + "." + (((bytTime[2] & 0x1) << 9) | (bytTime[1] << 1) | (bytTime[0] >> 7)).ToString("000") + "." + (((bytTime[0] & 0x7f)) * 10).ToString("000"); ;
+            }
+            catch(Exception ex)
+            {
+                return "00.00.00.00.000.000.000";
+            }
 
         }
 
@@ -218,7 +253,7 @@ namespace ParserBAAK12
                     {
                         if(data1[i,j]>=porogS+ Nul[i])
                         {
-                            Time[i] = j;
+                            Time[i] =j;
                             break;
                         }
 
